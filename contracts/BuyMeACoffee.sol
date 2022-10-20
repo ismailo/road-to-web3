0392f9c35cbc17e1a1e9887f5dfc7d22c193993a
ismailo <ismail.ish@gmail.com>
2022-10-20 13:43:05 -0700

    added code to BuyMeCoffee

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -22,5 +22,56 @@ contract BuyMeACoffee {
         string name;
         string message;
     }
+    // List of all memos received from friends.
+    Memo[] memos;
 
+    // Address of contract deployer 
+
+    // Deploy logic.
+    constructor {
+    owner = msg.sender;
+    
+    }
+
+
+    /** 
+    * @dev buy a coffee fro contract owener 
+    * @param _name name of hte coffee buyer 
+    * @param _message a nice message from the cofee buyer 
+     */
+    function buyCoffee(string mempory _name, string memory _message) public payable {
+        require (msg.value > 0, "can't but coffee with 0 eth");
+
+        Memo.push(Memo(
+            msg.sender.
+            block.timestamp,
+            _name,
+            _message
+        ));
+
+        //Emit a log event when a new memo is created 
+        emit NewMemo(
+            msg.sender.
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+
+    /**
+    * @dev send the entire balance stored in this contract tpo the owner
+    */
+
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+    * @dev retrieve all the memos received and stored on the blockchain 
+    */
+
+    function getMemos() public view returns(Memo[] memory) {
+        return memos;
+    }
 }
